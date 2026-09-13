cb66992cc7f68a5c6c337aa820868450f9e45089
Michael Oliver <mcoliver@gmail.com>
2026-09-13 16:13:20 -0700

    feat: bump dependencies to 26.2 (python 3.14.7, ffmpeg 9.0.1, oiio 3.1.17.0, boost 1.92.0, openexr 3.4.15)
    Signed-off-by: Michael Oliver <mcoliver@gmail.com>

--- ports/python3/vcpkg-port-config.cmake
+++ ports/python3/vcpkg-port-config.cmake
@@ -1,5 +1,5 @@
 include_guard(GLOBAL)
-set(PYTHON3_VERSION "3.14.5")
+set(PYTHON3_VERSION "3.14.7")
 set(PYTHON3_VERSION_MAJOR "3")
 set(PYTHON3_VERSION_MINOR "14")
 set(PYTHON3_INCLUDE "include/python3.14")
